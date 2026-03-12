3522c05c632aefc425dfe82ed4ce28990a2a7e99
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-03-13 00:47:20 +0530

    [Surbhi]:Added Address Book App Application

--- AddressBookApp/src/main/java/com/addressbookapp/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/addressbookapp/AddressBookAppApplication.java
@@ -2,6 +2,7 @@ package com.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
